a23b4ef171450f83f167ee17394fc5055a447646
SwordSongs <xiaoding_hupo@foxmail.com>
2023-07-28 23:03:51 +0800

    for_each

--- chapter11/main.cpp
+++ chapter11/main.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+void print(int elem)
+{
+    cout << elem << ' ';
+}
+
 int main()
 {
     vector<int> coll;
@@ -11,7 +16,24 @@ int main()
     cout << "for_each prints vector." << endl;
     for_each(coll.begin(), coll.end(), [](const int &elem) {cout << elem << ' '; });
     cout << endl;
+
+    cout << "for_each prints vector, by function." << endl;
+    for_each(coll.begin(), coll.end(), print);
+    cout << endl;
+
+    cout << "for_each , by reference." << endl;
+    for_each(coll.begin(), coll.end(), [](int &elem) {  elem += 10;  });
+    PRINT_ELEMENTS(coll);
     
+    for_each(coll.begin(), coll.end(), [=](int &elem) { elem += *coll.begin(); });
+    PRINT_ELEMENTS(coll);
+
+    for_each(coll.begin(), coll.end(), [&](int &elem) { elem += *coll.begin(); });
+    PRINT_ELEMENTS(coll);
+
+
+
+
     system("pause");
     return 0;
 }
